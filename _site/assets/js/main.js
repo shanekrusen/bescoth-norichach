347e35b62ca32e9bf68c1f1eee5034ae89f01cb9
Shane Krusen <shanekrusen@gmail.com>
2019-04-29 02:08:39 +0000

    Fixed dropdown again

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-  var header = document.getElementById("nav-links");
-  var sticky = header.offsetTop;
-  
-  if (!$( "#nav-links" ).is(":hidden")) {
-    window.onscroll = function() {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
-    }
-  }
-  
+$(document).ready(function() {  
   $( "#theology").click(function() {
     $( "#nav-drop" ).slideToggle();
     $( "#shader" ).toggle();
